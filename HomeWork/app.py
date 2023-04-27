efc42520812712b355fc9683c7ff47a8be4265b7
mareya83 <74158744+mareya83@users.noreply.github.com>
2023-04-27 22:19:47 +0300

    HomeWork

--- HomeWork/app.py
+++ HomeWork/app.py
@@ -1,8 +1,23 @@
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+
 
 import time
 
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
 driver.get('http://www.rozetka.ua')
+
+time.sleep(5)
+
+search_input = driver.find_element(By.CSS_SELECTOR, "input")
+search_input.send_keys("Samsung")
+
+time.sleep(5)
+
+ActionChains(driver).key_down(Keys.ENTER).perform()
+
+time.sleep(5)
